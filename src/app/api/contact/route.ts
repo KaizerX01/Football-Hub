2c7682e6c0653a88bf7dc6edf09aa9c2a1bc3593
KaizerX01 <tshensowgamer2004@google.com>
2025-05-04 17:17:49 +0100

    fix devios contact route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -5,9 +5,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { Resend } from 'resend';
 
 // Initialize the Resend client (with your API key)
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
     try {
         const { name, email,subject, message } = await req.json();
         
